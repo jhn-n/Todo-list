8edd39caf2616540821b1f42d71ed52caa678f3c
jhn-n <182117243+jhn-n@users.noreply.github.com>
2025-06-26 16:55:27 +0100

    add most functionality

---
+++ src/classes.js
@@ -0,0 +1,131 @@
+import { format } from "date-fns";
+
+export class Item {
+	constructor(title, description, dueDate, priority, completed, projectId) {
+		this.title = title;
+		this.description = description;
+		this.dueDate = new Date(dueDate);
+		this.priority = priority;
+		this.completed = completed;
+		this.projectId = projectId;
+		this.id = crypto.randomUUID();
+	}
+
+	edit(title, description, dueDate, priority, completed) {
+		this.title = title;
+		this.description = description;
+		this.dueDate = new Date(dueDate);
+		this.priority = priority;
+		this.completed = completed;
+	}
+}
+
+export class Project {
+	constructor(projectName) {
+		this.name = projectName;
+		this.items = [];
+		this.id = crypto.randomUUID();
+	}
+
+	// make private?
+	findItemIndex(itemId) {
+		const itemIndex = this.items.findIndex((e) => e.id === itemId);
+		if (itemIndex < 0) {
+			alert("could not find Item index for", itemId);
+			return undefined;
+		}
+		return itemIndex;
+	}
+
+	edit(name) {
+		this.name = name;
+	}
+
+	addItem(title, description, dueDate, priority, completed) {
+		const newItem = new Item(
+			title,
+			description,
+			dueDate,
+			priority,
+			completed,
+			this.id
+		);
+		this.items.push(newItem);
+	}
+
+	editItem(title, description, dueDate, priority, completed, itemId) {
+		const itemIndex = this.findItemIndex(itemId);
+		this.items[itemIndex].edit(
+			title,
+			description,
+			dueDate,
+			priority,
+			completed
+		);
+	}
+
+	removeItem(itemId) {
+		const itemIndex = this.findItemIndex(itemId);
+		this.items.splice(itemIndex, 1);
+	}
+}
+
+export class Workspace {
+	constructor(workplaceName) {
+		this.name = workplaceName;
+		this.projects = [];
+		this.addProject("Default");
+	}
+
+	findProjectIndex(projectId) {
+		const projectIndex = this.projects.findIndex((e) => e.id === projectId);
+		if (projectIndex < 0) {
+			alert("could not find project index for", projectId);
+			return undefined;
+		}
+		return projectIndex;
+	}
+
+	addProject(projectName) {
+		const newProject = new Project(projectName);
+		this.projects.push(newProject);
+	}
+
+	editProject(projectName, projectID) {
+		const projectIndex = this.findProjectIndex(projectID);
+		this.projects[projectIndex].edit(projectName);
+	}
+
+	removeProject(projectID) {
+		const projectIndex = this.findProjectIndex(projectID);
+		this.projects.splice(projectIndex, 1);
+	}
+
+	removeItem(projectID, itemID) {
+		const projectIndex = this.findProjectIndex(projectID);
+		this.projects[projectIndex].removeItem(itemID);
+	}
+
+	addItem(title, description, dueDate, priority, completed, projectID) {
+		const projectIndex = this.findProjectIndex(projectID);
+		this.projects[projectIndex].addItem(
+			title,
+			description,
+			dueDate,
+			priority,
+			completed
+		);
+	}
+
+	editItem(title, description, dueDate, priority, completed, projectID, itemID) {
+		const projectIndex = this.findProjectIndex(projectID);
+		this.projects[projectIndex].editItem(
+			title,
+			description,
+			dueDate,
+			priority,
+			completed,
+			itemID
+		);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,111 +1,188 @@
 import "./style.css";
-import { greeting } from "./dom.js";
-console.log(greeting);
+import { Item, Project, Workspace } from "./classes.js";
+import { format } from "date-fns";
 
-class Item {
-	constructor(title, description, dueDate, priority, completed, projectId) {
-		this.title = title;
-		this.description = description;
-		this.dueDate = dueDate;
-		this.priority = priority;
-		this.completed = completed;
-		this.projectId = projectId;
-		this.id = crypto.randomUUID();
+let currentItem, currentProject, usingDialogToEdit, projectUsingDialogtoEdit;
+const container = document.querySelector(".container");
+const itemDialog = document.querySelector("#itemDialog");
+const itemForm = document.querySelector("#itemForm");
+const formTitle = document.querySelector("#formTitle");
+const formDescription = document.querySelector("#formDescription");
+const formDueDate = document.querySelector("#formDueDate");
+const formPriority = document.querySelector("#formPriority");
+const formCompleted = document.querySelector("#formCompleted");
+const submitButton = document.querySelector("#submitButton");
+const cancelButton = document.querySelector("#cancelButton");
+const projectDialog = document.querySelector("#projectDialog");
+const projectForm = document.querySelector("#projectForm");
+const projectName = document.querySelector("#projectName");
+const addProjectButton = document.querySelector("#addProjectButton");
+const projectSubmitButton = document.querySelector("#projectSubmitButton");
+const projectCancelButton = document.querySelector("#projectCancelButton");
+
+function itemNode(item) {
+	const itemNode = document.createElement("ul");
+	const titleElement = document.createElement("li");
+	const descriptionElement = document.createElement("li");
+	const dueDateElement = document.createElement("li");
+	const priorityElement = document.createElement("li");
+	const completedElement = document.createElement("li");
+	const removeItemButton = document.createElement("button");
+	const editItemButton = document.createElement("button");
+
+	itemNode.classList.add("item");
+	titleElement.classList.add("title");
+	descriptionElement.classList.add("description");
+	dueDateElement.classList.add("dueDate");
+	priorityElement.classList.add("priority");
+	completedElement.classList.add("completed");
+
+	titleElement.innerText = item.title;
+	descriptionElement.innerText = item.description;
+	dueDateElement.innerText =
+		"Due by: " + format(item.dueDate, "EEE do MMM yyyy");
+	priorityElement.innerText = "Priority: " + item.priority;
+	completedElement.innerText = "Completed: " + item.completed;
+	removeItemButton.innerText = "remove";
+	editItemButton.innerText = "edit";
+
+	removeItemButton.addEventListener("click", () => {
+		myUser.removeItem(item.projectId, item.id);
+		display();
+	});
+
+	editItemButton.addEventListener("click", () => {
+		currentProject = item.projectId;
+		currentItem = item.id;
+		usingDialogToEdit = true;
+		formTitle.value = item.title;
+		formDescription.value = item.description;
+		formDueDate.value = item.dueDate;
+		formPriority.value = item.priority;
+		itemDialog.show();
+	});
+
+	itemNode.appendChild(titleElement);
+	itemNode.appendChild(descriptionElement);
+	itemNode.appendChild(dueDateElement);
+	itemNode.appendChild(priorityElement);
+	itemNode.appendChild(completedElement);
+	itemNode.appendChild(removeItemButton);
+	itemNode.appendChild(editItemButton);
+	return itemNode;
+}
+
+function projectNode(project) {
+	const projectNode = document.createElement("div");
+	const removeProjectButton = document.createElement("button");
+	const editProjectButton = document.createElement("button");
+	const projectName = document.createElement("h2");
+	const addItemButton = document.createElement("button");
+
+	projectNode.classList.add("project");
+	projectName.classList.add("name");
+
+	removeProjectButton.innerText = "remove";
+	editProjectButton.innerText = "edit";
+	projectName.innerText = project.name;
+	addItemButton.innerText = "add item";
+
+	removeProjectButton.addEventListener("click", () => {
+		myUser.removeProject(project.id);
+		display();
+	});
+
+	editProjectButton.addEventListener("click", () => {
+		currentProject = project.id;
+		projectUsingDialogtoEdit = true;
+		projectDialog.show();
+	});
+
+	addItemButton.addEventListener("click", () => {
+		currentProject = project.id;
+		usingDialogToEdit = false;
+		itemDialog.show();
+	});
+
+	projectNode.appendChild(removeProjectButton);
+	projectNode.appendChild(editProjectButton);
+	projectNode.appendChild(projectName);
+	for (const item of project.items) {
+		projectNode.appendChild(itemNode(item));
 	}
+	projectNode.appendChild(addItemButton);
+	return projectNode;
+}
 
-	edit(title, description, dueDate, priority, completed) {
-		this.title = title;
-		this.description = description;
-		this.dueDate = dueDate;
-		this.priority = priority;
-		this.completed = completed;
+function display() {
+	container.innerHTML = "";
+	for (const project of myUser.projects) {
+		container.appendChild(projectNode(project));
 	}
 }
 
-class Project {
-	constructor(projectName) {
-		this.name = projectName;
-		this.items = [];
-		this.id = crypto.randomUUID();
-	}
+const myUser = new Workspace("John");
 
-	// make private?
-	findItemIndex(itemId) {
-		const itemIndex = this.items.findIndex((e) => e.id === itemId);
-		if (itemIndex < 0) {
-			alert("could not find index");
-			return undefined;
-		}
-		return itemIndex;
-	}
+addProjectButton.addEventListener("click", () => {
+	projectUsingDialogtoEdit = false;
+	projectDialog.show();
+});
 
-	addItem(title, description, dueDate, priority, completed) {
-		const newItem = new Item(
-			title,
-			description,
-			dueDate,
-			priority,
-			completed,
-			this.id
+projectSubmitButton.addEventListener("click", (event) => {
+	if (projectUsingDialogtoEdit) {
+		myUser.editProject(formName.value, currentProject);
+	} else {
+		myUser.addProject(formName.value);
+	}
+	event.preventDefault();
+	projectForm.reset();
+	projectDialog.close();
+	display();
+});
+
+projectCancelButton.addEventListener("click", () => {
+	projectForm.reset();
+	projectDialog.close();
+});
+
+submitButton.addEventListener("click", (event) => {
+	if (usingDialogToEdit) {
+		myUser.editItem(
+			formTitle.value,
+			formDescription.value,
+			formDueDate.value,
+			formPriority.value,
+			formCompleted.checked,
+			currentProject,
+			currentItem
 		);
-		this.items.push(newItem);
-	}
-
-	editItem(title, description, dueDate, priority, completed, itemId) {
-		const itemIndex = this.findItemIndex(itemId);
-		this.items[itemIndex].edit(
-			title,
-			description,
-			dueDate,
-			priority,
-			completed
+	} else {
+		myUser.addItem(
+			formTitle.value,
+			formDescription.value,
+			formDueDate.value,
+			formPriority.value,
+			formCompleted.checked,
+			currentProject
 		);
 	}
+	event.preventDefault();
+	itemForm.reset();
+	itemDialog.close();
+	display();
+});
 
-	removeItem(itemId) {
-		const itemIndex = this.findItemIndex(itemId);
-		this.items.splice(itemIndex, 1);
-	}
-}
+cancelButton.addEventListener("click", () => {
+	itemForm.reset;
+	itemDialog.close();
+});
 
-class workspace {
-    constructor(workplaceName) {
-        this.name = workplaceName;
-        this.projects = [];
-        this.addProject("Default");
-    }
-
-    findProjectIndex(projectId) {
-        const projectIndex = this.projects.findIndex((e) => e.id === projectId);
-        if (itemIndex < 0) {
-            alert("could not find index");
-            return undefined;
-        }
-        return projectIndex;
-    }
-    
-    addProject(projectName) {
-        const newProject = new Project(projectName);
-        this.projects.push(newProject);
-    }
-    
-    removeProject(projectID) {
-        const projectIndex = this.findProjectIndex(projectID);
-        if (projectIndex < 0) {
-            alert("could not find index");
-        }
-        this.projects.splice(projectIndex, 1);
-    }
-}
-    
-const myUser = new workspace("John");
 myUser.addProject("Alternative");
-
 
 myUser.projects[0].addItem(
 	"Hair cut",
 	"Go to hairdressers",
-	"next week",
+	"2025/7/13",
 	"moderate",
 	false
 );
@@ -113,7 +190,7 @@ myUser.projects[0].addItem(
 myUser.projects[0].addItem(
 	"Clothes",
 	"Need new socks, shoes and coat",
-	"next month",
+	"2025/8/1",
 	"low",
 	false
 );
@@ -121,26 +198,22 @@ myUser.projects[0].addItem(
 myUser.projects[0].addItem(
 	"Book holiday",
 	"Research on websites",
-	"tomorrow",
+	"2025/6/30",
 	"high",
 	false
 );
 myUser.projects[1].addItem(
 	"Visit parents",
 	"Drive to hometown",
-	"next month",
+	"2025/7/10",
 	"moderate",
 	false
 );
 
+display(myUser);
 for (const project of myUser.projects) {
 	console.table(project);
 	for (const item of project.items) {
 		console.table(item);
 	}
 }
-
-const id1 = myUser.projects[0].items[0].id;
-const id2 = myUser.projects[0].items[1].id;
-
-
